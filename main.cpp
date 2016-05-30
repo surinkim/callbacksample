eabf4fee93fdd79ba8fa32bd900894392460af2b
surinkim <nnhope@hotmail.com>
2016-05-30 16:21:10 +0900

    Define CallbackBase class

--- main.cpp
+++ main.cpp
@@ -1,1 +1,25 @@
 #include "stdafx.h"
+
+#include <iostream>
+#include <Windows.h>
+
+enum { ACT_TYPE_CALLBACK = 1000 };
+
+struct StartResult
+{
+  enum { CALLBACK_TYPE = ACT_TYPE_CALLBACK + 1 };
+  int errorCode;
+};
+
+////////////////////////////////////////////////////
+class CallbackBase
+{
+public:
+  virtual ~CallbackBase() {};
+  
+  virtual void Invoke( void* param ) const = 0;
+  const int GetType() { return _callbackType; }
+  
+protected:
+  int _callbackType;
+};
